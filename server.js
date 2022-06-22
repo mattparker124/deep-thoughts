3884be303cc337c1f4c88c592b0e456d27b7d120
Matthew Parker <mattparker124@gmail.com>
2022-06-22 05:01:38 -0500

    set up basic server framework

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONBODB_URI || 'mongodb://localhost:27017/deep-thoughts', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
